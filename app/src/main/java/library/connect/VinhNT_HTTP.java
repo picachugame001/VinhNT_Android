e0f426b4e32f0f0b48c6e3821dc6deece98aa2bd
VinhNT <picachugame001@gmail.com>
2016-03-20 14:00:43 +0700

    add http support

--- app/src/main/java/library/connect/VinhNT_HTTP.java
+++ app/src/main/java/library/connect/VinhNT_HTTP.java
@@ -1,11 +1,15 @@
 package library.connect;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by Picachu on 3/20/2016.
  */
 public class VinhNT_HTTP {
     public static void sendRequest(){
-        //RequestQueue queue = Volley.newRequestQueue(this);
+        //RequestQueue queue = Volley.newRequestQueue();
         String url ="http://www.google.com";
     }
+
 }
